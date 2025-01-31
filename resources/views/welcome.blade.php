b299b1b007783e29064aadbbd4fc5aa92f04910e
CodeAldi <codealdi@gmail.com>
2025-02-01 01:53:11 +0700

    implementasi template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
-hello world
+@extends('layouts.main')
+@section('content')
+<div class="container mt-2">
+    <div class="card">
+        <h5 class="card-header">judul card</h5>
+        <div class="card-body">
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda voluptas, ut iure quo nostrum deserunt voluptatibus delectus? Odio fugit, dolorum tempore blanditiis quasi quaerat dicta sunt et adipisci quae libero animi odit, illo id commodi corrupti dignissimos eligendi consequatur, provident ipsum obcaecati illum iure! Cumque quod autem repudiandae. Dolorum, provident!</p>
+        </div>
+    </div>
+</div>
+@endsection
